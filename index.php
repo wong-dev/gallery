3e2e9e2eeeaafdd28eca3cf15cd96b565c7def40
Michael Wong <michaelwong306@gmail.com>
2021-03-01 23:29:33 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/gallery/node_modules/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="/gallery/assets/css/style.css">
+    <link rel="stylesheet" href="/gallery/node_modules/animate.css/animate.css">
+    <title>Gallery</title>
+</head>
+
+<body>
+    <div class="container-fluid mb-5">
+        <div class="row mt-5">
+            <div class="col col-md mb-4 ps-5">
+                <a href="https://www.haftcorpindo.com/wishlist/">
+                    <button type="button" class="btn text-white btn-info">
+                        <i class="fas fa-undo-alt"></i> Kembali
+                    </button>
+                </a>
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col">
+                <center>
+                    <h1 style="font-size: 4rem" class="title"><b>Memory Gallery</b></h1>
+                </center>
+            </div>
+        </div>
+    </div>
+    <div class="row-gallery" style="padding: 5rem;">
+        <div class="column">
+            <img src="/gallery/assets/images/gallery1.jpg">
+            <img src="/gallery/assets/images/gallery2.jpg">
+            <img src="/gallery/assets/images/gallery3.jpg">
+            <img src="/gallery/assets/images/gallery4.jpg">
+            <img src="/gallery/assets/images/gallery5.jpg">
+        </div>
+        <div class="column">
+            <img src="/gallery/assets/images/gallery8.jpg">
+            <img src="/gallery/assets/images/gallery9.jpg">
+            <img src="/gallery/assets/images/gallery10.jpg">
+            <img src="/gallery/assets/images/gallery11.jpg">
+            <img src="/gallery/assets/images/gallery12.jpg">
+            <img src="/gallery/assets/images/gallery13.jpg">
+        </div>
+        <div class="column">
+            <img src="/gallery/assets/images/gallery14.jpg">
+            <img src="/gallery/assets/images/gallery15.jpg">
+            <img src="/gallery/assets/images/gallery16.jpg">
+            <img src="/gallery/assets/images/gallery17.jpg">
+            <img src="/gallery/assets/images/gallery18.jpg">
+            <img src="/gallery/assets/images/gallery19.jpg">
+            <img src="/gallery/assets/images/gallery6.jpg">
+        </div>
+        <div class="column">
+            <img src="/gallery/assets/images/gallery21.jpg">
+            <img src="/gallery/assets/images/gallery22.jpg">
+            <img src="/gallery/assets/images/gallery23.jpg">
+            <img src="/gallery/assets/images/gallery24.jpg">
+            <img src="/gallery/assets/images/gallery25.jpg">
+            <img src="/gallery/assets/images/gallery26.jpg">
+            <img src="/gallery/assets/images/gallery7.jpg">
+            <img src="/gallery/assets/images/gallery20.jpg">
+        </div>
+    </div>
+
+    <script src="/gallery/assets/scripts/vanilla.js"></script>
+</body>
+
+</html>
